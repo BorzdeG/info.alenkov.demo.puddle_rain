77d2154abc50cc5ba529012d7c03573800028519
Victor Alenkov <Victor@Alenkov.info>
2013-11-02 14:29:36 +0400

    [rus] 0) Исправлены ошибки 1) Добавлены тестовые модели 2) Исправлен алгоритм расчёта 3) Навёл красоты в коде
    [eng]
    0) Fixed
    1) Added the test models
    2) The algorithm of calculation
    3) Navel Beauty in code

--- src/main/java/info/alenkov/demo/puddle_rain/Calculate.java
+++ src/main/java/info/alenkov/demo/puddle_rain/Calculate.java
@@ -6,59 +6,148 @@ import org.slf4j.LoggerFactory;
 public class Calculate {
 	private transient static final Logger LOG = LoggerFactory.getLogger(Calculate.class.getName());
 
-	public static void main(String[] args) {
-		Integer[] data = {2, 5, 1, 2, 3, 4, 7, 7, 6};
-		LOG.info("result: {}", calculateVolume(data));
+	private static int v; // Объём воды
+	private static int v_u; // Объём воды над "ватерлинией"
+	private static int v_d; // Объём воды под "ватерлинией"
+
+	private static int s_l; // Сколько "прошагали" от левой границы
+	private static int s_pl; // Сколько "прошагали" от максимальной высоты "под водой"
+
+	private static int h_l; // Высота "стартовой" левой границы
+	private static int h_p; // Предыдущая высота
+	private static int h_pl; // Максимальная высота "под водой"
+	private static int h_c; // Высота на текущем шаге
+
+
+	private static void breathedAir() {
+		LOG.debug("Вдохнули воздуха");
+
+		// Сбрасываем счётчики
+		v += v_u + v_d;
+		v_u = 0;
+		v_d = 0;
+		s_l = -1;
+		s_pl = -1;
+		// И начинаем движение с новой точки
+		h_l = h_c;
+		h_pl = h_c;
+	}
+
+	private static void movingUnderWaterUp() {
+		LOG.debug("Всё выше и выше и выше");
+
+		int h_d = (h_c > h_pl) ? ((h_c - h_pl) * s_l) : ((h_pl == h_l) ? (h_c - h_p) : 0);
+		LOG.debug("h_d: {}", h_d);
+
+		if (h_c >= h_pl) {
+			h_pl = h_c;
+			s_pl = -1;
+		}
+		v_u += (h_l - h_pl) - h_d;
+		v_d += Math.abs(h_c - h_pl) + h_d;
+	}
+
+	private static void movingUnderWaterDown() {
+		LOG.debug("В глубины пучины");
+
+		if (h_pl == h_l) {
+			v_u += h_l - h_c;
+			if (s_l == 0) {
+				h_pl = h_c;
+			}
+		} else {
+
+			int h_d = Math.abs(h_c - h_pl);
+			v_u += h_l - h_pl;
+			v_d += h_d;
+		}
+	}
+
+	private static void movingUnderWater() {
+		LOG.debug("Двигаемся под водой");
+
+		if (h_c > h_p) {
+			movingUnderWaterUp();
+		} else if (h_c < h_l) {
+			movingUnderWaterDown();
+		}
+	}
+
+	private static void alignHeightWater() {
+		LOG.debug("Сливаем излишки");
+
+		int d = 0;
+
+		if (h_pl > h_c && s_l == s_pl) {
+			v_u = 0;
+			v_d = 0;
+		} else if (h_pl > h_c) {
+			LOG.debug("Шлюз 0");
+			v_u -= (h_l - h_pl) * s_l;
+
+			LOG.debug("Шлюз 1");
+			v_d -= (h_pl - h_c) * s_pl;
+		} else {
+			v_u -= (h_l - h_c) * s_l;
+		}
+		LOG.debug("d: {}", d);
+
+		v += v_u + v_d;
+		v_u = 0;
+		v_d = 0;
+
+		h_l = h_c;
+		h_pl = h_c;
+		s_l = -1;
+		s_pl = -1;
 	}
 
 	public static int calculateVolume(Integer[] data) {
-		int volume = 0;
-		int t_volume = 0;
 
-		int h_l = data[0]; // Высота "стартовой" левой границы
-		int h_p = h_l; // Предыдущая высота
-		int h_r = h_l; // Высота правой границы
-		int s = 0; // Сколько "прощли" по X от левой до правой границы
-		boolean b = false; // Признак наличия "впадины"
+		h_l = data[0];
+		h_p = data[0];
+		h_pl = data[0];
+		h_c = data[0];
+		int h_r = data[data.length - 1]; // Высота правой границы
+
+		v_u = 0;
+		v_d = 0;
+		v = 0;
+
+		s_l = 0;
+		s_pl = 0;
+
+		LOG.debug("--- 0 ---");
+		LOG.debug("h_l: {}; h_c: {}; h_p: {}; h_pl: {}; h_r: {}", h_l, h_c, h_p, h_pl, h_r);
 
 		for (int i = 1; i < data.length; i++) {
-			h_r = data[i];
-			LOG.debug("left: {}; prev: {}; right: {}; b: {}", h_l, h_p, h_r, b);
+			h_c = data[i];
+			LOG.debug("--- {} ---", i);
+			LOG.debug("s_l: {}; s_pl: {}", s_l, s_pl);
+			LOG.debug("h_l: {}; h_c: {}; h_p: {}; h_pl: {}; h_r: {}", h_l, h_c, h_p, h_pl, h_r);
 
-			if (h_r != h_p) {
-				LOG.debug("Дно изменяет высоту");
-				s++;
-				if (h_r < h_l) {
-					LOG.debug("Правая граница всё ещё под водой");
-					t_volume += h_l - h_r;
-					b = h_r > h_p || b;
-				} else if (h_r > h_p) {
-					LOG.debug("Правая граница над водой");
-					volume += t_volume;
-					t_volume = 0;
-					h_l = h_r;
-					s = 0;
-					b = false;
-				}
+			if (h_c >= h_l) {
+				breathedAir();
+			} else if (h_c > h_r && s_pl > 0) {
+				alignHeightWater();
+			} else if (h_c < h_l) {
+				movingUnderWater();
 			} else {
-				LOG.debug("Плоское дно");
-				t_volume += h_l - h_r;
+				LOG.error("Неучтённая ситуация");
 			}
-			h_p = h_r;
 
-			LOG.debug("volume: {}; t_volume: {}", volume, t_volume);
+			h_p = h_c;
+			s_l++;
+			s_pl++;
+
+			LOG.debug("v_u: {}; v_d: {}; v: {}", v_u, v_d, v);
+		}
+		if (h_l > h_r) {
+			alignHeightWater();
 		}
 
-		if (h_r < h_l) {
-			LOG.debug("Убираем 'излишки' воды");
-			LOG.debug("left: {}; prev: {}; right: {}; b: {}", h_l, h_p, h_r, b);
-			if (b) {
-				final int d = (h_l - h_p) * s;
-				LOG.debug("излишки: {}", d);
-				volume += t_volume - d;
-			}
-		}
+		LOG.debug("v_u: {}; v_d: {}; v: {}", v_u, v_d, v);
 
-		return volume;
+		return v;
 	}
 }
--- src/test/java/info/alenkov/demo/puddle_rain/CalculateTest.java
+++ src/test/java/info/alenkov/demo/puddle_rain/CalculateTest.java
@@ -10,32 +10,73 @@ import org.testng.annotations.Test;
 public class CalculateTest {
 	private transient static final Logger LOG = LoggerFactory.getLogger(CalculateTest.class.getName());
 
+	final Object[] data0  = {new Integer[]{2, 5, 1, 2, 3, 4, 7, 7, 6}, 10};
+	final Object[] data1  = {new Integer[]{2, 5, 1, 3, 1, 2, 1, 7, 7, 6}, 17};
+	final Object[] data2  = {new Integer[]{5, 1, 3, 6, 1, 6, 1, 3, 1, 4}, 18};
+	final Object[] data3  = {new Integer[]{1, 2, 3, 4, 5, 5, 4, 3, 2, 1}, 0};
+	final Object[] data4  = {new Integer[]{6, 1, 1, 1, 7, 1, 1, 1, 1, 7}, 39};
+	final Object[] data5  = {new Integer[]{7, 1, 1, 1, 7, 1, 1, 1, 1, 7}, 42};
+	final Object[] data6  = {new Integer[]{1, 2, 3, 4, 5, 6, 7, 8}, 0};
+	final Object[] data7  = {new Integer[]{8, 7, 6, 5, 4, 3, 2, 1}, 0};
+	final Object[] data8  = {new Integer[]{5, 1, 3, 6, 1, 5, 1, 7, 6, 5}, 17};
+	final Object[] data9  = {new Integer[]{7, 1, 3, 6, 1, 5, 1, 7, 6, 5}, 25};
+	final Object[] data10 = {new Integer[]{3, 4, 7, 3, 4, 7, 6, 7, 2, 4}, 10};
+	final Object[] data11 = {new Integer[]{5, 1, 4, 2, 3}, 4};
+	final Object[] data12 = {new Integer[]{6, 1, 5, 2, 1, 4}, 9};
+	final Object[] data13 = {new Integer[]{4, 1, 2, 5, 1, 6}, 9};
+	final Object[] data14 = {new Integer[]{7, 1, 5, 2, 1, 4}, 9};
+	final Object[] data15 = {new Integer[]{7, 1, 5, 2, 1, 4, 2}, 9};
+	final Object[] data16 = {new Integer[]{7, 1, 5, 2, 1, 4, 3, 2}, 9};
+	final Object[] data17 = {new Integer[]{7, 1, 5, 2, 1, 4, 3, 2}, 9};
+	final Object[] data18 = {new Integer[]{5, 1, 5, 1, 5, 1, 5}, 12};
+	final Object[] data19 = {new Integer[]{1, 5, 1, 5, 1, 5, 1}, 8};
+	final Object[] data20 = {new Integer[]{5, 1, 3}, 2};
+	final Object[] data21 = {new Integer[]{0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1}, 0};
+
 	@DataProvider
-	public Object[][] data() {
-		final Object[] data0 = {new Integer[]{2, 5, 1, 2, 3, 4, 7, 7, 6}, 10};
-		final Object[] data1 = {new Integer[]{2, 5, 1, 3, 1, 2, 1, 7, 7, 6}, 17};
-		final Object[] data2 = {new Integer[]{5, 1, 3, 6, 1, 6, 1, 3, 1, 4}, 18};
-		final Object[] data3 = {new Integer[]{1, 2, 3, 4, 5, 5, 4, 3, 2, 1}, 0};
-		final Object[] data4 = {new Integer[]{6, 1, 1, 1, 7, 1, 1, 1, 1, 7}, 39};
-		final Object[] data5 = {new Integer[]{7, 1, 1, 1, 7, 1, 1, 1, 1, 7}, 42};
-		final Object[] data6 = {new Integer[]{1, 2, 3, 4, 5, 6, 7, 8}, 0};
-		final Object[] data7 = {new Integer[]{8, 7, 6, 5, 4, 3, 2, 1}, 0};
-		final Object[] data8 = {new Integer[]{5, 1, 3, 6, 1, 5, 1, 7, 6, 5}, 17};
-		final Object[] data9 = {new Integer[]{7, 1, 3, 6, 1, 5, 1, 7, 6, 5}, 25};
-		final Object[] data10 = {new Integer[]{3, 4, 7, 3, 4, 7, 6, 7, 2, 4}, 10};
+	public Object[][] dataAll() {
+		return new Object[][]{data0,
+		                      data1,
+		                      data2,
+		                      data3,
+		                      data4,
+		                      data5,
+		                      data6,
+		                      data7,
+		                      data8,
+		                      data9,
+		                      data10,
+		                      data11,
+		                      data12,
+		                      data13,
+		                      data14,
+		                      data15,
+		                      data16,
+		                      data17,
+		                      data18,
+		                      data19,
+		                      data20,
+		                      data21};
+	}
 
-
-		return new Object[][]{data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10};
+	@DataProvider
+	public Object[][] dataOne() {
+		return new Object[][]{data9};
 	}
 
 	@BeforeMethod
 	public void setUp() throws Exception {
 		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
-		LOG.error("------------------------");
+		LOG.debug("---------");
 	}
 
-	@Test(dataProvider = "data")
-	public void testCalculateVolume(Integer[] data, int volume) throws Exception {
+	@Test(dataProvider = "dataAll", dependsOnMethods = "testCalculateVolumeOne", enabled = true)
+	public void testCalculateVolumeAll(Integer[] data, int volume) throws Exception {
+		Assert.assertEquals(Calculate.calculateVolume(data), volume);
+	}
+
+	@Test(dataProvider = "dataOne")
+	public void testCalculateVolumeOne(Integer[] data, int volume) throws Exception {
 		Assert.assertEquals(Calculate.calculateVolume(data), volume);
 	}
 }
